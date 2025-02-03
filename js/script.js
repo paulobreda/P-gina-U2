6975e7ac54b3c48d3052e490e1c9f5f60f072e48
Paulo Breda <97465748+paulobreda@users.noreply.github.com>
2025-02-03 18:26:20 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+d// scripts.js
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 4000); // Muda de slide a cada 4 segundos
+}
+
+function plusSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    slideIndex += n;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
